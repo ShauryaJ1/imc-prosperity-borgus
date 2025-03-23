74c55902ebabbcabfc1aa2690253e9582d53ab84
Shaurya Jain <shaurya.s.jain@gmail.com>
2025-03-23 12:29:30 -0400

    tried mcginley

--- main.py
+++ main.py
@@ -23,6 +23,9 @@ class Trader:
         self.ema_prices = {
             product:None for product in self.products
         }
+        self.mcginley_prices = {
+            product:None for product in self.products
+        }
     def get_price(self,symbol, state: TradingState):
         if self.ema_prices[symbol] is None:
             default_price = self.default_prices[symbol]
@@ -40,6 +43,12 @@ class Trader:
                 self.ema_prices[symbol] = mid_price
             else:
                 self.ema_prices[symbol] = self.ema_param * mid_price + (1-self.ema_param) * self.ema_prices[symbol]
+    def update_mcginley(self,symbol,state):
+        mid_price = self.get_price(symbol, state)
+        if self.mcginley_prices[symbol] is None:
+            self.mcginley_prices[symbol] = mid_price
+        else:
+            self.mcginley_prices[symbol] = self.mcginley_prices[symbol]+(mid_price-self.mcginley_prices[symbol])/(0.6*10*(mid_price/self.mcginley_prices[symbol])**4)
     def resin_trading(self,state:TradingState):
         if "RAINFOREST_RESIN" not in state.position:
             current_position = 0
@@ -52,7 +61,60 @@ class Trader:
         orders.append(Order("RAINFOREST_RESIN", self.default_prices["RAINFOREST_RESIN"] - 2, bid_volume))
         orders.append(Order("RAINFOREST_RESIN", self.default_prices["RAINFOREST_RESIN"] + 2, ask_volume))
         return orders
-    def kelp_trading(self,state:TradingState):
+    def kelp_trading_combined(self,state:TradingState):
+        if "KELP" not in state.position:
+            current_position = 0
+        else:
+            current_position = state.position["KELP"]
+        bid_volume = self.position_limit["KELP"] - current_position
+        ask_volume = - self.position_limit["KELP"] - current_position
+
+        orders = []
+        combined_price = 1.1*self.ema_prices["KELP"] - 0.1*self.mcginley_prices["KELP"]
+        if current_position == 0:
+            # Not long nor short
+            orders.append(Order("KELP", math.floor(combined_price - 1), bid_volume))
+            orders.append(Order("KELP", math.ceil(combined_price + 1), ask_volume))
+        
+        if current_position > 0:
+            # Long position
+            orders.append(Order("KELP", math.floor(combined_price - 2), bid_volume))
+            orders.append(Order("KELP", math.ceil(combined_price), ask_volume))
+
+        if current_position < 0:
+            # Short position
+            orders.append(Order("KELP", math.floor(combined_price), bid_volume))
+            orders.append(Order("KELP", math.ceil(combined_price+ 2), ask_volume))
+
+        return orders
+    def kelp_trading_mcginley(self,state:TradingState):
+        if "KELP" not in state.position:
+            current_position = 0
+        else:
+            current_position = state.position["KELP"]
+        bid_volume = self.position_limit["KELP"] - current_position
+        ask_volume = - self.position_limit["KELP"] - current_position
+
+        orders = []
+
+        if current_position == 0:
+            # Not long nor short
+            orders.append(Order("KELP", math.floor(self.mcginley_prices["KELP"] - 1), bid_volume))
+            orders.append(Order("KELP", math.ceil(self.mcginley_prices["KELP"] + 1), ask_volume))
+        
+        if current_position > 0:
+            # Long position
+            orders.append(Order("KELP", math.floor(self.mcginley_prices["KELP"] - 2), bid_volume))
+            orders.append(Order("KELP", math.ceil(self.mcginley_prices["KELP"]), ask_volume))
+
+        if current_position < 0:
+            # Short position
+            orders.append(Order("KELP", math.floor(self.mcginley_prices["KELP"]), bid_volume))
+            orders.append(Order("KELP", math.ceil(self.mcginley_prices["KELP"] + 2), ask_volume))
+
+        return orders
+    
+    def kelp_trading_ema(self,state:TradingState):
         if "KELP" not in state.position:
             current_position = 0
         else:
@@ -80,13 +142,18 @@ class Trader:
         return orders
     
     def run(self, state: TradingState):
-        print("Beginning of run")
-        print(self.position_limit)
+        # print("Beginning of run")
+        # print(self.position_limit)
         # Only method required. It takes all buy and sell orders for all symbols as an input, and outputs a list of orders to be sent
         for product in self.products:
-            self.update_ema_prices(product, state)
+            # self.update_ema_prices(product, state)
+            self.update_mcginley(product, state)
+        # print(self.mcginley_prices)
+        print(self.mcginley_prices["KELP"])
+
+        # print("Mid price of : ", self.get_price)
         orders = {}        
-        orders["KELP"] = self.kelp_trading(state)
+        orders["KELP"] = self.kelp_trading_mcginley(state)
         # orders.extend(self.resin_trading(state))
         orders["RAINFOREST_RESIN"] = self.resin_trading(state)
         

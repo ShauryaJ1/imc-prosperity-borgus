427d882c39b4175fb3e56eafbc6d479479364b7c
Shaurya Jain <shaurya.s.jain@gmail.com>
2025-03-31 14:02:28 -0400

    fix risk/spread factoring

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ import string
 import math
 class Trader:
     def __init__(self):
+        self.risk = 0.10
+        self.profit_target = {"KELP": 2, "RAINFOREST_RESIN": 1}
         self.position_limit = {
             "KELP": 50,
             "RAINFOREST_RESIN": 50
         }
-        self.ema_param = 0.55
+        self.volumes = {
+            "KELP": 5,
+            "RAINFOREST_RESIN": 10
+        }
+        self.ema_param = 0.5
         self.default_prices = {
             "KELP":2000,
             "RAINFOREST_RESIN":10000
@@ -26,16 +32,26 @@ class Trader:
         self.mcginley_prices = {
             product:None for product in self.products
         }
+        # self.spreads = {
+        #     product:0 for product in self.products
+        # }
+        self.spreads = {
+            "KELP": 4,
+            "RAINFOREST_RESIN": 7
+        }
     def get_price(self,symbol, state: TradingState):
-        if self.ema_prices[symbol] is None:
-            default_price = self.default_prices[symbol]
-        else:
-            default_price = self.ema_prices[symbol]
-        if symbol not in state.order_depths:
-            return default_price
-        if len(state.order_depths[symbol].buy_orders) == 0 or len(state.order_depths[symbol].sell_orders) == 0:
-            return default_price
+        # if self.ema_prices[symbol] is None:
+        #     default_price = self.default_prices[symbol]
+        # else:
+        #     default_price = self.ema_prices[symbol]
+        # if symbol not in state.order_depths:
+        #     return default_price
+        # if len(state.order_depths[symbol].buy_orders) == 0 or len(state.order_depths[symbol].sell_orders) == 0:
+        #     return default_price
         return (max(state.order_depths[symbol].buy_orders)+ min(state.order_depths[symbol].sell_orders))/2
+    def update_spreads(self, symbol, state):
+        for symbol in self.products:
+            self.spreads[symbol] = min(state.order_depths[symbol].sell_orders) - max(state.order_depths[symbol].buy_orders) + self.profit_target[symbol]
     def update_ema_prices(self, symbol, state):
             mid_price = self.get_price(symbol, state)
 
@@ -60,6 +76,19 @@ class Trader:
         orders = []
         orders.append(Order("RAINFOREST_RESIN", self.default_prices["RAINFOREST_RESIN"] - 2, bid_volume))
         orders.append(Order("RAINFOREST_RESIN", self.default_prices["RAINFOREST_RESIN"] + 2, ask_volume))
+        return orders
+    def resin_risk_spread(self,state:TradingState):
+        if "RAINFOREST_RESIN" not in state.position:
+            current_position = 0
+        else:
+            current_position = state.position["RAINFOREST_RESIN"]
+        bid_volume = self.position_limit["RAINFOREST_RESIN"] - current_position
+        ask_volume = - self.position_limit["RAINFOREST_RESIN"] - current_position
+        # bid_volume = self.volumes["RAINFOREST_RESIN"]
+        # ask_volume = self.volumes["RAINFOREST_RESIN"]
+        orders = []
+        orders.append(Order("RAINFOREST_RESIN", math.floor(self.default_prices["RAINFOREST_RESIN"] - 2 - self.spreads["RAINFOREST_RESIN"]/2), bid_volume))
+        orders.append(Order("RAINFOREST_RESIN", math.ceil(self.default_prices["RAINFOREST_RESIN"] + 2 + self.spreads["RAINFOREST_RESIN"]/2), ask_volume))
         return orders
     def kelp_trading_combined(self,state:TradingState):
         if "KELP" not in state.position:
@@ -113,7 +142,33 @@ class Trader:
             orders.append(Order("KELP", math.ceil(self.mcginley_prices["KELP"] + 2), ask_volume))
 
         return orders
-    
+    def kelp_trading_mcginley_risk_spread(self,state:TradingState):
+        if "KELP" not in state.position:
+            current_position = 0
+        else:
+            current_position = state.position["KELP"]
+        bid_volume = self.position_limit["KELP"] - current_position
+        ask_volume = - self.position_limit["KELP"] - current_position
+        # bid_volume = self.volumes["KELP"]
+        # ask_volume = self.volumes["KELP"]
+        orders = []
+
+        if current_position == 0:
+            # Not long nor short
+            orders.append(Order("KELP", math.floor(self.mcginley_prices["KELP"] - 1 -self.spreads["KELP"]/2 ), bid_volume))
+            orders.append(Order("KELP", math.ceil(self.mcginley_prices["KELP"] + 1 + self.spreads["KELP"]/2), ask_volume))
+        
+        if current_position > 0:
+            # Long position
+            orders.append(Order("KELP", math.floor(self.mcginley_prices["KELP"] - 2 -current_position*self.risk - self.spreads["KELP"]/2), bid_volume))
+            orders.append(Order("KELP", math.ceil(self.mcginley_prices["KELP"] + -current_position*self.risk + self.spreads["KELP"]/2), ask_volume))
+
+        if current_position < 0:
+            # Short position
+            orders.append(Order("KELP", math.floor(self.mcginley_prices["KELP"]  -current_position*self.risk - self.spreads["KELP"]/2), bid_volume))
+            orders.append(Order("KELP", math.ceil(self.mcginley_prices["KELP"] + 2 -current_position*self.risk + self.spreads["KELP"]/2), ask_volume))
+
+        return orders
     def kelp_trading_ema(self,state:TradingState):
         if "KELP" not in state.position:
             current_position = 0
@@ -148,9 +203,11 @@ class Trader:
         for product in self.products:
             # self.update_ema_prices(product, state)
             self.update_mcginley(product, state)
+            # self.update_spreads(product, state)
         # print(self.mcginley_prices)
-        print(self.mcginley_prices["KELP"])
-
+        # print(self.mcginley_prices["KELP"])
+        print(min(state.order_depths["KELP"].sell_orders) - max(state.order_depths["KELP"].buy_orders) + self.profit_target["KELP"])
+        print(min(state.order_depths["RAINFOREST_RESIN"].sell_orders) - max(state.order_depths["RAINFOREST_RESIN"].buy_orders) + self.profit_target["RAINFOREST_RESIN"])
         # print("Mid price of : ", self.get_price)
         orders = {}        
         orders["KELP"] = self.kelp_trading_mcginley(state)
